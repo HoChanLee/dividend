84d4366a516fc68125dbdd574faf921ee6ffbd43
leehochan <leehc0427@naver.com>
2023-07-07 17:03:33 +0900

    first commet

---
+++ src/main/java/com/dayone/Application.java
@@ -0,0 +1,19 @@
+package com.dayone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+import java.io.IOException;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableCaching
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
